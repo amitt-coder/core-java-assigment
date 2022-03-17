ef70a582020a07e22d0be7a1008c1c31d7d84e34
Amit Patel <ap81511@gmail.com>
2022-03-17 12:01:18 +0530

    rename package1

---
+++ src/com/controlstatement/ass/SwitchCase.java
@@ -0,0 +1,19 @@
+package com.ControlStatement.ass;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		
+		int day = 2 ;
+		
+		switch(day) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+			default:
+				System.out.println("Not a valid value");
+		}
+	}
+}
---
+++ src/com/controlstatement/ass/Value.java
@@ -0,0 +1,16 @@
+package com.ControlStatement.ass;
+
+public class Value {
+	public static void main(String[] args) {
+		
+		int day = 2;
+		
+		if(day == 1) {
+			System.out.println("Monday");
+		} else if(day == 2) {
+			System.out.println("Tuesday");
+		} else {
+			System.out.println("Not a valid value");
+		}
+	}
+}
